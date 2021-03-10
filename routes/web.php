5fc62ef460b0919e1ea26dec0303807947748f06
Richard Meitern <richard.meitern@gmail.com>
2021-03-10 14:05:30 +0200

    added welcome route

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\ExcelController;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('welcome');
+})->name('blank');
 
 Route::get('/tere', function () {
     return view('welcome');
