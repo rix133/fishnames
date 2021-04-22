9cd7604bdc46dd6ae715e6e3a94ecf0ed0b4a3a7
Richard Meitern <richard.meitern@gmail.com>
2021-04-22 15:35:39 +0300

    added usage instructions

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('EMI Kalanimede andmebaas') }}
+            {{ __('Liiginimede rakenduse kasutusjuhend') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="aspect-w-16 aspect-h-9">
+                    <iframe src="https://www.youtube-nocookie.com/embed/videoseries?list=PLq-iZzpuwQFCm1k45U0-W17ZePSd5JnJR&rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
             </div>
         </div>
     </div>  
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,5 +22,5 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio'), require('@tailwindcss/typography')],
 };
